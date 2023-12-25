18a5ee2b6a466212820e6c367775f74ea5a9a964
DeokJae Kim <kdj0712@gmail.com>
2023-12-25 17:23:08 +0900

    add docs

--- docs/quests/jobplanet_login_selector.py
+++ docs/quests/jobplanet_login_selector.py
@@ -15,13 +15,15 @@ browser.get("https://www.jobplanet.co.kr/users/sign_in")
 from selenium.webdriver.common.by import By
 element_login_field = browser.find_element(by=By.CSS_SELECTOR, value ="#user_email")
 element_login_field.send_keys("kdj0712@hanmail.net")
+# 지정한 field에 내가 입력한 특정 key값이 입력되도록 한다.
 
 element_password_field = browser.find_element(by=By.CSS_SELECTOR, value= "#user_password")
 element_password_field.send_keys("********")
-
+# 지정한 field에 내가 입력한 특정 key값이 입력되도록 한다.
 
 element_login_button = browser.find_element(by=By.CSS_SELECTOR, value= "section.section_email.er_r > fieldset > button")
 element_login_button.click
+# 지정한 위치의 버튼을 클릭하도록 한다.
 pass
 # 브라우저 종료
 browser.quit()
