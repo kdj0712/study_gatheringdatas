295140e9d477b2ddd25c9e30d535afc5d7f44999
DeokJae Kim <kdj0712@gmail.com>
2023-12-26 12:19:20 +0900

    quest

---
+++ docs/selenium/quests/coupang_paginations_selectors.py
@@ -0,0 +1,26 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+chrome_options = Options()
+
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+
+webdriver_manager_directory = ChromeDriverManager().install()
+
+browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory),options=chrome_options)
+
+capabilities = browser.capabilities
+
+from selenium.webdriver.common.by import By
+
+for page_number in range(1,11) :  # page number
+    url = "https://www.coupang.com/np/campaigns/348?page={}".format(page_number)
+    browser.get(url)
+    time.sleep(3)
+    html = browser.page_source        
+    pass                                 
+
+
