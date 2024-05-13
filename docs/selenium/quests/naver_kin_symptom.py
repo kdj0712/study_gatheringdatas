89eb6a1eea603e83a2899128cc0eac1f1545df87
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-05-13 23:42:34 +0900

    commit

--- docs/selenium/quests/naver_kin_symptom.py
+++ docs/selenium/quests/naver_kin_symptom.py
@@ -20,8 +20,8 @@ driver = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
 capabilities = driver.capabilities
 collection = Connect()
 dise_list = [
-    # {"유전성근병증NOS" : "G71.9"}, {"멜라스증후군" : "G71.3"}, {"네말린근병증" : "G71.2"},
-    # {"근세관성(중심핵성)근병증" :"G71.2"}, {"근섬유형 불균형":"G71.2"}, {"워커-워버그 증후군":"G71.2"}, {"선천성근병증" : "G71.2"},
+    {"멜라스증후군" : "G71.3"}, {"네말린근병증" : "G71.2"},
+    {"근세관성(중심핵성)근병증" :"G71.2"}, {"근섬유형 불균형":"G71.2"}, {"워커-워버그 증후군":"G71.2"}, {"선천성근병증" : "G71.2"},
     # {"근디스트로피 NOS": "G71.2"}, {"근긴장증 NOS":"G71.1"}, {"근긴장장애":"G71.1"}, {"근긴장디스트로피[스타이너트]" :"G71.1"},
     # {"우성 선천성 근긴장증" : "G71.1"}, {"열성 선천성 근긴장증" : "G71.1"}, {"선천성 이상근긴장증" : "G71.1"},
     # {"근디스트로피": "G71.0"}, {"만성 염증성 탈수초성 다발신경병증" : "G61.8"},
@@ -165,6 +165,7 @@ for dise in dise_list:
     counts = math.ceil(total / 12)
     
     if counts == 1:
+        current_page = 1
         table = driver.find_elements(by=By.CSS_SELECTOR, value ="#mainContainer > div.content > div.section_home_search")
         articles = driver.find_elements(by=By.CSS_SELECTOR, value ="div.search_item_wrap > div.item_list > div > div")
         origin_tab = driver.current_window_handle
@@ -174,6 +175,7 @@ for dise in dise_list:
             con_title.click()
             all_tabs = driver.window_handles
             new_tab = [tab for tab in all_tabs if tab != origin_tab][0]
+            driver.switch_to.window(new_tab)
             try:
                 WebDriverWait(driver, 10).until(
                 EC.frame_to_be_available_and_switch_to_it((By.NAME, 'cafe_main'))
@@ -284,7 +286,11 @@ for dise in dise_list:
                         f.write(f'{dise_name},{last_page}')
                 else:
                     print("리스트의 끝에 도달했습니다.")
-            break
+                    all_done = True  # 모든 처리 완료
+                    break
+        if all_done:
+            break  # 바깥쪽 반복문 종료
+
 
 
     elif counts > 1:
@@ -428,7 +434,7 @@ for dise in dise_list:
                         if dise_name in dise:
                             current_index = i
                             break
-                    
+                        
                     # 다음 dise_name과 dise_code 가져오기
                     if current_index is not None:
                         next_index = current_index + 1
